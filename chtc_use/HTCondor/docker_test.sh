c7733b883e15bef5a83a3b3b442223f7109febf8
Alyssa Anastasi <alyssarose91402@gmail.com>
2025-10-22 13:09:03 -0500

    Add CHTC docker tests

---
+++ chtc_use/HTCondor/docker_test.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Function to print a command and then evaluate it
+run_cmd() {
+    local cmd=$1
+    echo "=> $cmd"
+    eval $cmd
+    echo ""
+}
+
+data_dir="HAVEN/output/raw/kuzmin/binary/haven"
+
+######
+# If running on docker
+source /venv/docker_haven_venv/bin/activate
+####
+
+echo ""
+echo "1. Login to wandb"
+echo "--------------------------------------------"
+echo ""
+
+run_cmd "export WANDB_API_KEY=9892595596b389b2400e1a3ae3bef9ee86cbf936"
+run_cmd "wandb login"
+
+
+echo ""
+echo "1. Check Pip List"
+echo "--------------------------------------------"
+echo ""
+
+run_cmd "pip list"
