06a77bf512c3b1f37253fe8730bb32dc6a935043
Alyssa Anastasi <alyssarose91402@gmail.com>
2025-10-22 13:21:19 -0500

    Fix docker path

--- chtc_use/HTCondor/docker_test.sh
+++ chtc_use/HTCondor/docker_test.sh
@@ -12,7 +12,7 @@ data_dir="HAVEN/output/raw/kuzmin/binary/haven"
 
 ######
 # If running on docker
-source /venv/docker_haven_venv/bin/activate
+source /venv/docker_haven/bin/activate
 ####
 
 echo ""
