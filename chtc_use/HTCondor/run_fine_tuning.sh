5f48b3c26ab80cbafc5f17eea2c6165b5e91d44a
Alyssa Anastasi <alyssarose91402@gmail.com>
2025-10-29 13:03:06 -0500

    update sub file for LOOCV

--- chtc_use/HTCondor/run_fine_tuning.sh
+++ chtc_use/HTCondor/run_fine_tuning.sh
@@ -45,7 +45,7 @@ run_cmd "ls $data_dir"
 # run_cmd "ls mgm/mgm/data"
 # run_cmd "ls $data_dir"
 
-run_cmd "mv $data_dir /staging/aranastasi/HAVEN/outputs/"
+run_cmd "mv $data_dir /staging/aranastasi/HAVEN/outputs/LOOCV"
 
 # delete output from the job working directory
 # run_cmd "rm -r $data_dir"
